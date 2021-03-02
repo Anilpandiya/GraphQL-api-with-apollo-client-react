deebe4f6bc8db7b0a2afedf9d5ad232816926e1a
Anil pandiya <anilpandiya05@gmail.com>
2021-03-02 16:49:40 +0530

    M-Step4: Mutation cache updating

--- app/src/pages/conference/Speakers.jsx
+++ app/src/pages/conference/Speakers.jsx
@@ -41,6 +41,7 @@ const FEATURED_SPEAKER = gql `
   mutation markFeatured($speakerId: ID!, $featured: Boolean!){
     markFeatured(speakerId: $speakerId, featured: $featured){
       id
+      featured
     }
   }
 `;
